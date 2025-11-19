738672deaf9d4c96634ebddf5b65c22cbdce9519
Aleksandr Pavlyuk <a.pavlyuk@vkteam.ru>
2025-11-19 11:24:56 +0300

    fix tg analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,19 @@ import App from "./App";
 import "./index.css";
 import {queryClient} from "./config/queryClient";
 import {HashRouter} from "react-router-dom";
+import TelegramAnalytics from "@telegram-apps/analytics";
 
 const manifestUrl = `https://tagwaiter.ru/tonconnect-manifest-newest.json`;
 const appUrl = "https://t.me/CryptoSplitBot?startapp"
+const tgAnalyticsToken = import.meta.env.VITE_TELEGRAM_ANALYTICS_TOKEN;
+const tgAnalyticsAppName = import.meta.env.VITE_TELEGRAM_ANALYTICS_APP_NAME;
+
+if (tgAnalyticsToken && tgAnalyticsAppName) {
+  TelegramAnalytics.init({
+    token: tgAnalyticsToken,
+    appName: tgAnalyticsAppName,
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider 
