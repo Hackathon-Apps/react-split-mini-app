f3484f5786c5b0c703b86dd490faa535ea3dcb58
Alex-pvl <sasha.pavlyuk.1996@inbox.ru>
2025-11-30 13:40:59 +0700

    replace logo

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import {queryClient} from "./config/queryClient";
 import {HashRouter} from "react-router-dom";
 import TelegramAnalytics from "@telegram-apps/analytics";
 
-const manifestUrl = `https://tagwaiter.ru/tonconnect-manifest-newest.json`;
+const manifestUrl = `https://tagwaiter.ru/tonconnect-manifest-new-one.json`;
 const appUrl = "https://t.me/CryptoSplitBot?startapp"
 const tgAnalyticsToken = import.meta.env.VITE_TELEGRAM_ANALYTICS_TOKEN;
 const tgAnalyticsAppName = import.meta.env.VITE_TELEGRAM_ANALYTICS_APP_NAME;
